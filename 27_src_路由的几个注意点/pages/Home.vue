31499504b61ca6795a9ffd8bc09c726a83c39047
魏东城 <18621756389@163.com>
2022-06-11 15:48:13 +0800

    Vue2学习vueRouter的几个注意点

---
+++ 27_src_路由的几个注意点/pages/Home.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <h2>我是Home的内容</h2>
+  </div>
+</template>
+
+<script>
+  export default {
+    name:'Home',
+    /*beforeDestroy() {
+      console.log('Home组件即将被销毁了')
+    },*/
+    mounted() {
+      console.log('Home组件挂载完毕',this)
+      window.homeRoute = this.$route
+      window.homeRouter = this.$router
+    }
+  }
+</script>
