cdf92dd2fee3d92bceaab4b2dd8d3ddd74fe1655
魏东城 <18621756389@163.com>
2022-06-12 11:13:38 +0800

    Vue2学习vueRouter的全局路由守卫

---
+++ 37_src_全局路由守卫/App.vue
@@ -0,0 +1,31 @@
+<template>
+  <div id="root">
+    <div>
+      <Banner/>
+    </div>
+    <div>
+      <div class="col-xs-2 col-xs-offset-2">
+        <div class="list-group">
+          <router-link class="list-group-item" active-class="active" :to="{name:'guanyu'}">About</router-link>
+          <router-link class="list-group-item" active-class="active" to="/home">Home</router-link>
+        </div>
+      </div>
+      <div class="col-xs-6">
+        <div class="panel">
+          <div class="panel-body">
+              <router-view/>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+
+</template>
+<script>
+    import Banner from "@/components/Banner";
+    export default {
+      name:'App',
+      components: {Banner}
+    }
+</script>
+
