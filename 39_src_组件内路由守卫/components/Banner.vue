f1afbfaa9436c518f03262b43937637c32808f8c
魏东城 <18621756389@163.com>
2022-06-12 14:57:37 +0800

    Vue2学习vueRouter的组件内路由守卫

---
+++ 39_src_组件内路由守卫/components/Banner.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="col-xs-offset-2 col-xs-8">
+    <div class="page-header">
+      <h2>Vue Router Demo</h2>
+      <button @click="back">后退</button>
+      <button @click="forward">前进</button>
+      <button @click="go">测试一下go</button>
+    </div>
+  </div>
+</template>
+
+<script>
+  export default {
+    name:'Banner',
+    methods:{
+      back(){
+        //后退
+        this.$router.back()
+      },
+      forward(){
+        //前进
+        this.$router.forward()
+      },
+      go(){
+        //正数:前进几步，负数:后退几步
+        this.$router.go(-2)
+      }
+    }
+  }
+</script>
---
+++ 39_src_组件内路由守卫/main.js
@@ -0,0 +1,18 @@
+//引入Vue
+import Vue from "vue";
+//引入App组件
+import App from "./App";
+//引入VueRouter
+import VueRouter from "vue-router";
+//引入路由器
+import router from "@/router";
+//关闭生产提示
+Vue.config.productionTip = false
+//应用插件
+Vue.use(VueRouter)
+
+new Vue({
+    el:"#app",
+    render:h=>h(App),
+    router
+})
