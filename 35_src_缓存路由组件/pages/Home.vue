726e4392d0396e322f5e60bc45ae8a0734b8b038
魏东城 <18621756389@163.com>
2022-06-12 10:01:49 +0800

    Vue2学习vueRouter的缓存路由组件

---
+++ 35_src_缓存路由组件/pages/Home.vue
@@ -0,0 +1,33 @@
+<template>
+  <div>
+    <h2>我是Home的内容</h2>
+    <div>
+      <ul class="nav nav-tabs">
+        <li>
+          <router-link  class="list-group-item" active-class="active" to="/home/news">News</router-link>
+        </li>
+        <li>
+          <router-link  class="list-group-item" active-class="active" to="/home/message">Message</router-link>
+        </li>
+      </ul>
+      <!-- 缓存路由,include属性对应的值为组件名称 -->
+      <keep-alive include="News">
+        <router-view/>
+      </keep-alive>
+    </div>
+  </div>
+</template>
+
+<script>
+  export default {
+    name:'Home',
+    /*beforeDestroy() {
+      console.log('Home组件即将被销毁了')
+    },*/
+    /*mounted() {
+      console.log('Home组件挂载完毕',this)
+      window.homeRoute = this.$route
+      window.homeRouter = this.$router
+    }*/
+  }
+</script>
---
+++ 35_src_缓存路由组件/pages/News.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <li>news001 <input teype="text"/></li>
+    <li>news002 <input teype="text"/></li>
+    <li>news003 <input teype="text"/></li>
+  </div>
+</template>
+
+<script>
+  export default {
+    name:'News',
+    beforeDestroy() {
+      console.log('News组件即将被销毁')
+    }
+  }
+</script>
