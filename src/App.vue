b6881190c95357bb38a74ed492e74c5ecc6bb5b1
魏东城 <18621756389@163.com>
2022-06-12 16:36:47 +0800

    vue前端UI组件的应用

--- src/App.vue
+++ src/App.vue
@@ -1,37 +1,34 @@
 <template>
   <div>
-    <div>
-      <div class="col-xs-offset-2 col-xs-8">
-        <div class="page-header"><h2>Vue Router Demo</h2></div>
-      </div>
-    </div>
-    <div>
-      <div class="col-xs-2 col-xs-offset-2">
-        <div class="list-group">
-        <!-- 原始html中我们使用a标签实现页面跳转 -->
-        <!-- <a class="list-group-item active" href="./about.html">About</a>
-          <a class="list-group-item" href="./home.html">Home</a>-->
+    <button>原生的按钮</button>
+    <input type="text"/>
 
-          <!-- Vue中借助router-link标签实现路由的切换 -->
-          <router-link class="list-group-item" active-class="active" to="/about">About</router-link>
-          <router-link class="list-group-item" active-class="active" to="/home">Home</router-link>
-        </div>
-      </div>
-      <div class="col-xs-6">
-        <div class="panel">
-          <div class="panel-body">
-            <!-- 指定组件视图的呈现位置 -->
-            <router-view/>
-          </div>
-        </div>
-      </div>
-    </div>
+    <atguigu-row>
+      <atguigu-button>默认按钮</atguigu-button>
+      <atguigu-button type="primary">主要按钮</atguigu-button>
+      <atguigu-button type="success">成功按钮</atguigu-button>
+      <atguigu-button type="info">信息按钮</atguigu-button>
+      <atguigu-button type="warning">警告按钮</atguigu-button>
+      <atguigu-button type="danger">危险按钮</atguigu-button>
+    </atguigu-row>
+    <atguigu-date-picker
+        type="date"
+        placeholder="选择日期">
+    </atguigu-date-picker>
+    <atguigu-row>
+      <atguigu-button icon="el-icon-search" circle></atguigu-button>
+      <atguigu-button type="primary" icon="el-icon-edit" circle></atguigu-button>
+      <atguigu-button type="success" icon="el-icon-check" circle></atguigu-button>
+      <atguigu-button type="info" icon="el-icon-message" circle></atguigu-button>
+      <atguigu-button type="warning" icon="el-icon-star-off" circle></atguigu-button>
+      <atguigu-button type="danger" icon="el-icon-delete" circle></atguigu-button>
+    </atguigu-row>
   </div>
 
 </template>
 <script>
     export default {
-        name:'App'
+      name:'App',
     }
 </script>
 
--- src/main.js
+++ src/main.js
@@ -2,17 +2,27 @@
 import Vue from "vue";
 //引入App组件
 import App from "./App";
-//引入VueRouter
-import VueRouter from "vue-router";
-//引入路由器
-import router from "@/router";
+
+//完整引入
+//引入ElementUI组件库
+// import ElementUI from 'element-ui';
+//引入ElementUI全部样式
+// import 'element-ui/lib/theme-chalk/index.css';
+
+//按需引入
+import { Button,Row,DatePicker} from 'element-ui';
+
+Vue.component('atguigu-button', Button);
+Vue.component('atguigu-row', Row);
+Vue.component('atguigu-date-picker', DatePicker);
+
 //关闭生产提示
 Vue.config.productionTip = false
-//应用插件
-Vue.use(VueRouter)
+
+//应用ElementUI
+// Vue.use(ElementUI)
 
 new Vue({
     el:"#app",
-    render:h=>h(App),
-    router
+    render:h=>h(App)
 })
